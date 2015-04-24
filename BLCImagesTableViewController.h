de9bda28fc4db17aa9982214123770ae43b7e6d6
Leah Padgett <lmp0024@yahoo.com>
2015-04-24 12:35:36 -0400

    Table view controller implemented v1

---
+++ BLCImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Leah Padgett on 4/22/15.
+//  Copyright (c) 2015 Leah Padgett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
