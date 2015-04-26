3cf442939b8b5998f26e5f16f8144a18e0a0c49e
Leah Padgett <lmp0024@yahoo.com>
2015-04-26 15:28:36 -0400

    Refactored user names and captions

---
+++ BLCMedia.m
@@ -0,0 +1,14 @@
+//
+//  BLCMedia.m
+//  Blocstagram
+//
+//  Created by Leah Padgett on 4/26/15.
+//  Copyright (c) 2015 Leah Padgett. All rights reserved.
+//
+
+#import "BLCMedia.h"
+#import "BLCUser.h"
+
+@implementation BLCMedia
+
+@end
